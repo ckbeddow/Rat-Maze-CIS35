bd41f159522ff2b2cd5d0dcb7ff80fc55b03ab04
ckbeddow <charles.beddow@gmail.com>
2017-02-13 12:08:31 -0800

    Added Key and Barrier Prefabs with a KeyUnlock script as well as an exampel scene to see how they work.

---
+++ Assets/Scripts/KeyUnlock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyUnlock : MonoBehaviour {
+
+	public GameObject barrier;
+
+	void OnTriggerEnter(Collider other) {
+		//If the player enters the key's collider then disable the barrier
+		if (other.gameObject.CompareTag ("Player")) {
+			this.gameObject.SetActive (false);
+			Collider bar = barrier.GetComponent (typeof(Collider)) as Collider;
+			bar.enabled = false;
+		}
+	}
+
+}
