86dce8e1845cb825f34c4c7bb1510518dc44da01
Clara Olson <clara.olson22@gmail.com>
2017-04-12 13:54:27 -0700

    Block in wall on level Basic 2.0 now turns off the electric floor for a number of seconds before reactivating it. Block/lever is currently activated by the rat running into it.

---
+++ Assets/Scripts/LeverPress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverPress : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject floor;
+	public float time = 5.0f;
+
+	void OnTriggerEnter(Collider other) {
+		//If the player hits the button turn off the floor
+		if (other.gameObject.CompareTag ("Player")) {
+			StartCoroutine(ShutOff());
+		}		
+	}
+
+	IEnumerator ShutOff() {
+		floor.SetActive (false);
+		yield return new WaitForSeconds (time);
+		floor.SetActive (true);
+	}
+		
+}
